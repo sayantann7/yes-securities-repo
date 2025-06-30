326e08d5a3dc12a40ed07317d18390e73c1cdaf3
Sayantan Nandi <sayantannandi13@gmail.com>
2025-06-30 20:13:22 +0530

    added font everywhere

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,31 @@ import { AuthProvider } from '@/context/AuthContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
+import { Text } from 'react-native';
+import { typography } from '@/constants/font';
+
+const customTextProps = {
+  style: {
+    fontWeight: '900',
+    fontFamily: typography.primary,
+    color: '#002EDC',
+    textShadowColor: 'rgba(0, 46, 220, 0.3)',
+    textShadowOffset: { width: 0.5, height: 0.5 },
+    textShadowRadius: 0.5
+  }
+};
+
+const originalRender = (Text as any).render;
+(Text as any).render = function render(props: any) {
+  const mergedProps = { ...props, style: [customTextProps.style, props.style] };
+  return originalRender.apply(this, [mergedProps]);
+};
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
-    'BookAnitque': require('../assets/fonts/book-antiqua.ttf'),
+    [typography.primary]: require('../assets/fonts/book-antiqua.ttf'),
   });
   useEffect(() => {
     if (fontsLoaded || fontError) {
